31c002d3cfd9aa8249ab0cb0195f75cf8b27de8d
Antonio González <antonio.gonzalez13@utp.ac.pa>
2025-07-24 23:29:43 -0500

    Mejora de BotFacil

--- AjedrezJava/src/BotFacil.java
+++ AjedrezJava/src/BotFacil.java
@@ -1,20 +1,37 @@
-public class BotFacil {
+import java.util.Random;
 
+public class BotFacil {
+    private static final Random RAND = new Random();
+
+    /**
+     * Recorre todas las piezas negras y, usando reservoir sampling,
+     * selecciona uniformemente un movimiento válido sin almacenar todos.
+     * @param piezas Estado actual del tablero
+     * @return int[]{filaOrigen, colOrigen, filaDestino, colDestino} o null si no hay movimientos
+     */
     public static int[] obtenerMovimiento(String[][] piezas) {
-        for (int filaOrigen = 0; filaOrigen < 8; filaOrigen++) {
-            for (int colOrigen = 0; colOrigen < 8; colOrigen++) {
-                String pieza = piezas[filaOrigen][colOrigen];
+        int[] movimientoSeleccionado = null;
+        int conteoMovimientos = 0;
+
+        for (int filaO = 0; filaO < 8; filaO++) {
+            for (int colO = 0; colO < 8; colO++) {
+                String pieza = piezas[filaO][colO];
                 if (pieza != null && pieza.contains("negro")) {
-                    for (int filaDestino = 0; filaDestino < 8; filaDestino++) {
-                        for (int colDestino = 0; colDestino < 8; colDestino++) {
-                            if (ValidadorMovimiento.esMovimientoValido(piezas, pieza, filaOrigen, colOrigen, filaDestino, colDestino)) {
-                                return new int[]{filaOrigen, colOrigen, filaDestino, colDestino};
+                    for (int filaD = 0; filaD < 8; filaD++) {
+                        for (int colD = 0; colD < 8; colD++) {
+                            if (ValidadorMovimiento.esMovimientoValido(piezas, pieza, filaO, colO, filaD, colD)) {
+                                conteoMovimientos++;
+                                // Con probabilidad 1/conteoMovimientos, reemplazo la elección anterior
+                                if (RAND.nextInt(conteoMovimientos) == 0) {
+                                    movimientoSeleccionado = new int[]{filaO, colO, filaD, colD};
+                                }
                             }
                         }
                     }
                 }
             }
         }
-        return null;
+
+        return movimientoSeleccionado;
     }
 }
